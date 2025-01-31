ff168aaa0829eb57adea7fb1d920b5a57cbc0f5c
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-01-31 18:53:56 -0300

    ai sim

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:organizamais/page/login/login_page.dart';
 
+import 'controller/auth_controller.dart';
 import 'routes/route.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -15,6 +19,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Get.put(AuthController());
+
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
