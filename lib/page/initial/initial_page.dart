daf372545a8347831dacfa4fa6be53fa0947a075
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-03 12:05:45 -0300

    dmk

--- lib/page/initial/initial_page.dart
+++ lib/page/initial/initial_page.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:organizamais/utils/color.dart';
 
 class InitialPage extends StatelessWidget {
   const InitialPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Tela Iniciefegrgal'),
+    return Scaffold(
+      backgroundColor: DefaultColors.background,
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 20.w,
+              horizontal: 20.h,
+            ),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: DefaultColors.white,
+                    borderRadius: BorderRadius.circular(14.r),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text("Total"),
+                      Text("R\$ 4000,00"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
