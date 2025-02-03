dadef0eaff5af2cf540a71ef3f117942b03ee6e1
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-03 14:54:12 -0300

    ta no rumo

--- lib/page/initial/initial_page.dart
+++ lib/page/initial/initial_page.dart
@@ -4,6 +4,7 @@ import 'package:organizamais/page/initial/widget/finance_summary.dart';
 import 'package:organizamais/utils/color.dart';
 
 import 'widget/fixed_accounts.dart';
+import 'widget/goal_progress_card.dart';
 
 class InitialPage extends StatelessWidget {
   const InitialPage({super.key});
@@ -25,7 +26,17 @@ class InitialPage extends StatelessWidget {
                 SizedBox(
                   height: 20.h,
                 ),
-                FixedAccounts()
+                FixedAccounts(),
+                SizedBox(
+                  height: 20.h,
+                ),
+                GoalProgressCard(
+                  title: "Nubank",
+                  subtitle: "vence em dia 15",
+                  progress: 0.55, // 55%
+                  startDate: "0",
+                  endDate: "4000",
+                )
               ],
             ),
           ),
