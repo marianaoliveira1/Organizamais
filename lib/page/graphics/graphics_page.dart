c0463e5dfe6897e87424bec7a4467a015d019ec9
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-04 16:05:27 -0300

    oi

--- lib/page/graphics/graphics_page.dart
+++ lib/page/graphics/graphics_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:organizamais/utils/color.dart';
 
 class GraphicsPage extends StatelessWidget {
   const GraphicsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Graphics Page'),
+    return Scaffold(
+      backgroundColor: DefaultColors.background,
     );
   }
 }
