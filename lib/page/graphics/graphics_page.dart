81a49fb0c225a05f478429dd76bcfd4c271a578e
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-05 15:23:31 -0300

    .

--- lib/page/graphics/graphics_page.dart
+++ lib/page/graphics/graphics_page.dart
@@ -1,4 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:organizamais/utils/color.dart';
 
 class GraphicsPage extends StatelessWidget {
@@ -8,6 +10,32 @@ class GraphicsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: DefaultColors.background,
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 20.h,
+              horizontal: 16.w,
+            ),
+            decoration: BoxDecoration(
+              color: DefaultColors.white,
+              borderRadius: BorderRadius.circular(24.r),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "Total",
+                  style: TextStyle(
+                    color: DefaultColors.grey,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
