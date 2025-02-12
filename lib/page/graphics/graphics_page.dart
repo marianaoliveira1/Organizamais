2737b3b210160cf355b399df30de4dc308e3142d
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-12 17:48:19 -0300

    ijifdksla

--- lib/page/graphics/graphics_page.dart
+++ lib/page/graphics/graphics_page.dart
@@ -1,11 +1,57 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:organizamais/utils/color.dart';
 
 class GraphicsPage extends StatelessWidget {
   const GraphicsPage({super.key});
+
+  Widget _buildPieChart() {
+    return SizedBox(
+      height: 200.h,
+      child: PieChart(
+        PieChartData(
+          sectionsSpace: 0,
+          centerSpaceRadius: 40,
+          sections: [
+            PieChartSectionData(
+              value: 35,
+              color: Colors.blue,
+              title: '35%',
+              radius: 50,
+              titleStyle: TextStyle(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.bold,
+                color: DefaultColors.white,
+              ),
+            ),
+            PieChartSectionData(
+              value: 40,
+              color: Colors.green,
+              title: '40%',
+              radius: 50,
+              titleStyle: TextStyle(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.bold,
+                color: DefaultColors.white,
+              ),
+            ),
+            PieChartSectionData(
+              value: 25,
+              color: Colors.orange,
+              title: '25%',
+              radius: 50,
+              titleStyle: TextStyle(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.bold,
+                color: DefaultColors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +64,8 @@ class GraphicsPage extends StatelessWidget {
         ),
         child: Column(
           children: [
-            SizedBox(
-              height: 200.h,
-              child: CustomBarChart(),
-            ),
+            _buildPieChart(),
+            SizedBox(height: 20.h),
             Container(
               padding: EdgeInsets.symmetric(
                 vertical: 20.h,
@@ -66,12 +110,14 @@ class GraphicsPage extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           Text("R\$ 4000,00"),
-                          Text("5%",
-                              style: TextStyle(
-                                color: DefaultColors.grey,
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.w500,
-                              )),
+                          Text(
+                            "5%",
+                            style: TextStyle(
+                              color: DefaultColors.grey,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
                         ],
                       )
                     ],
@@ -82,87 +128,6 @@ class GraphicsPage extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class CustomBarChart extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: DefaultColors.background,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: BarChart(
-          BarChartData(
-            alignment: BarChartAlignment.spaceAround,
-            maxY: 300,
-            barTouchData: BarTouchData(
-              touchTooltipData: BarTouchTooltipData(
-                tooltipPadding: EdgeInsets.all(8),
-                tooltipMargin: 8,
-                tooltipRoundedRadius: 8,
-                tooltipBorder: BorderSide(color: Colors.black),
-                getTooltipItem: (group, groupIndex, rod, rodIndex) {
-                  return BarTooltipItem(
-                    '-\$${rod.toY.toStringAsFixed(2)}',
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                  );
-                },
-              ),
-            ),
-            titlesData: FlTitlesData(
-              leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  getTitlesWidget: (value, meta) {
-                    List<String> months = [
-                      'Jan',
-                      'Feb',
-                      'Mar',
-                      'Apr',
-                      'May',
-                      'Jun',
-                      'Jul',
-                      'Aug'
-                    ];
-                    return Text(months[value.toInt()], style: TextStyle(fontWeight: FontWeight.bold));
-                  },
-                ),
-              ),
-            ),
-            gridData: FlGridData(show: false),
-            borderData: FlBorderData(show: false),
-            barGroups: [
-              _buildBar(0, 100, Colors.pinkAccent),
-              _buildBar(1, 284, Colors.pink),
-              _buildBar(2, 150, Colors.pinkAccent),
-              _buildBar(3, 170, Colors.pink),
-              _buildBar(4, 200, Colors.pinkAccent),
-              _buildBar(5, 80, Colors.pink),
-              _buildBar(6, 90, Colors.pinkAccent),
-              _buildBar(7, 120, Colors.pink),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  BarChartGroupData _buildBar(int x, double y, Color color) {
-    return BarChartGroupData(
-      x: x,
-      barRods: [
-        BarChartRodData(
-          toY: y,
-          width: 16,
-          borderRadius: BorderRadius.circular(6), // Deixa as bordas arredondadas
-          color: color,
-        ),
-      ],
     );
   }
 }
