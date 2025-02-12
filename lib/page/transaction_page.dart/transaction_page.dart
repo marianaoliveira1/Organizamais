be5c1ed2c8895a9b8bed358e2d42ef7e2016700b
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-12 20:36:56 -0300

    ele abriu o mar vermelho pro povo passar

--- lib/page/transaction_page.dart/transaction_page.dart
+++ lib/page/transaction_page.dart/transaction_page.dart
@@ -50,11 +50,15 @@ class _TransactionPageState extends State<TransactionPage> {
         fontWeight: FontWeight.w500,
       ),
       decoration: InputDecoration(
-        hintText: "A data da sua conquista",
+        hintText: "Data",
         hintStyle: TextStyle(
           fontSize: 16.sp,
           color: DefaultColors.grey,
           fontWeight: FontWeight.w500,
+        ),
+        prefixIcon: Icon(
+          Icons.calendar_month,
+          color: DefaultColors.black,
         ),
         focusedBorder: const UnderlineInputBorder(
           borderSide: BorderSide(color: DefaultColors.black),
@@ -184,44 +188,77 @@ class PaymentTypeField extends StatelessWidget {
   void _showPaymentOptions(BuildContext context) {
     showModalBottomSheet(
       context: context,
+      backgroundColor: DefaultColors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(
           top: Radius.circular(20.r),
         ),
       ),
-      builder: (context) => Container(
-        padding: EdgeInsets.all(16.w),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Selecione o método de pagamento',
-              style: TextStyle(
-                fontSize: 18.sp,
-                fontWeight: FontWeight.w600,
-                color: DefaultColors.black,
-              ),
+      isScrollControlled: true, // Permite que o modal ocupe mais espaço
+      builder: (context) => Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom, // Ajusta conforme teclado
+        ),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(16.w),
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // Evita que a Column ocupe mais espaço do que precisa
+              children: [
+                Text(
+                  'Selecione o método de pagamento',
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 10.h),
+                ListView(
+                  shrinkWrap: true, // Evita overflow
+                  physics: NeverScrollableScrollPhysics(), // Desativa rolagem dupla
+                  children: [
+                    _buildPaymentOption(
+                      context,
+                      'Dinheiro',
+                      'assets/icon-payment/money.png',
+                      controller,
+                    ),
+                    _buildPaymentOption(
+                      context,
+                      'PIX',
+                      'assets/icon-payment/pix.png',
+                      controller,
+                    ),
+                    _buildPaymentOption(
+                      context,
+                      'Boleto',
+                      'assets/icon-payment/fatura.png',
+                      controller,
+                    ),
+                    _buildPaymentOption(
+                      context,
+                      'Criptomoedas',
+                      'assets/icon-payment/cifrao.png',
+                      controller,
+                    ),
+                    _buildPaymentOption(
+                      context,
+                      'Vale Refeição',
+                      'assets/icon-payment/cartoes-de-credito.png',
+                      controller,
+                    ),
+                    _buildPaymentOption(
+                      context,
+                      'TED',
+                      'assets/icon-payment/ted.png',
+                      controller,
+                    ),
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 20.h),
-            _buildPaymentOption(
-              context,
-              'Cartão',
-              Icons.credit_card,
-              controller,
-            ),
-            _buildPaymentOption(
-              context,
-              'Dinheiro',
-              Icons.attach_money,
-              controller,
-            ),
-            _buildPaymentOption(
-              context,
-              'PIX',
-              Icons.pix,
-              controller,
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -230,23 +267,38 @@ class PaymentTypeField extends StatelessWidget {
   Widget _buildPaymentOption(
     BuildContext context,
     String title,
-    IconData icon,
+    String assetPath,
     TextEditingController controller,
   ) {
-    return ListTile(
-      leading: Icon(icon, color: DefaultColors.black),
-      title: Text(
-        title,
-        style: TextStyle(
-          fontSize: 16.sp,
-          color: DefaultColors.black,
-          fontWeight: FontWeight.w500,
-        ),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.r),
+        color: DefaultColors.background,
       ),
-      onTap: () {
-        controller.text = title;
-        Navigator.pop(context);
-      },
+      padding: EdgeInsets.symmetric(
+        vertical: 4.h,
+        horizontal: 10.w,
+      ),
+      margin: EdgeInsets.only(bottom: 14.h),
+      child: ListTile(
+        leading: Image.asset(
+          assetPath,
+          width: 22.w,
+          height: 22.h,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 14.sp,
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        onTap: () {
+          controller.text = title;
+          Navigator.pop(context);
+        },
+      ),
     );
   }
 
@@ -257,19 +309,19 @@ class PaymentTypeField extends StatelessWidget {
       readOnly: true,
       style: TextStyle(
         fontSize: 16.sp,
-        color: DefaultColors.black,
+        color: Colors.black,
         fontWeight: FontWeight.w500,
       ),
       decoration: InputDecoration(
         hintText: "Selecione o tipo de pagamento",
         hintStyle: TextStyle(
           fontSize: 16.sp,
-          color: DefaultColors.grey,
+          color: Colors.grey,
           fontWeight: FontWeight.w500,
         ),
-        suffixIcon: Icon(Icons.arrow_drop_down, color: DefaultColors.black),
+        suffixIcon: Icon(Icons.arrow_drop_down, color: Colors.black),
         focusedBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: DefaultColors.black),
+          borderSide: BorderSide(color: Colors.black),
         ),
       ),
       onTap: () => _showPaymentOptions(context),
