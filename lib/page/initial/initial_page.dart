1bb885e2756c31dcf33fc529bc52aeef41d42228
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-17 18:35:08 -0300

    if

--- lib/page/initial/initial_page.dart
+++ lib/page/initial/initial_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:organizamais/page/initial/widget/finance_summary.dart';
+import 'package:organizamais/page/transaction/widget/title_transaction.dart';
 
 import 'package:organizamais/utils/color.dart';
 
@@ -133,159 +134,153 @@ class CreditCardSection extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Widget de Meus Cartões
-            Card(
-              elevation: 4,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Meus Cartões',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Meus Cartões',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
-                        IconButton(
-                          icon: Icon(Icons.add_circle, color: Colors.blue),
-                          onPressed: () {
-                            Get.to(() => AddCardPage(
-                                  isEditing: false,
-                                ));
-                          },
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 8),
-                    Obx(() {
-                      // Verificamos se já temos cartões carregados
-                      if (cardController.card.isEmpty) {
-                        return Container(
-                          height: 150,
-                          alignment: Alignment.center,
-                          child: Text('Nenhum cartão adicionado'),
-                        );
-                      } else {
-                        return Container(
-                          height: 150,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: cardController.card.length,
-                            itemBuilder: (context, index) {
-                              final card = cardController.card[index];
-                              return Padding(
-                                padding: const EdgeInsets.only(right: 16.0),
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Get.to(() => AddCardPage(
-                                          isEditing: true,
-                                          card: card,
-                                        ));
-                                  },
-                                  child: Container(
-                                    width: 200,
-                                    decoration: BoxDecoration(
-                                      color: Colors.blue.shade700,
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    padding: EdgeInsets.all(16),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          children: [
-                                            if (card.iconPath != null)
-                                              Image.asset(
-                                                card.iconPath!,
-                                                width: 40,
-                                                height: 40,
-                                              ),
-                                            SizedBox(width: 8),
-                                            Expanded(
-                                              child: Text(
-                                                card.name,
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 16,
-                                                ),
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.add, color: DefaultColors.grey),
+                        onPressed: () {
+                          Get.to(
+                            () => AddCardPage(
+                              isEditing: false,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Obx(() {
+                    // Verificamos se já temos cartões carregados
+                    if (cardController.card.isEmpty) {
+                      return Container(
+                        height: 150,
+                        alignment: Alignment.center,
+                        child: Text('Nenhum cartão adicionado'),
+                      );
+                    } else {
+                      return Container(
+                        height: 150,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: cardController.card.length,
+                          itemBuilder: (context, index) {
+                            final card = cardController.card[index];
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 16.0),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Get.to(() => AddCardPage(
+                                        isEditing: true,
+                                        card: card,
+                                      ));
+                                },
+                                child: Container(
+                                  width: 200,
+                                  padding: EdgeInsets.all(16),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          if (card.iconPath != null)
+                                            Image.asset(
+                                              card.iconPath!,
+                                              width: 40,
+                                              height: 40,
                                             ),
-                                          ],
-                                        ),
-                                        Spacer(),
-                                        Text(
-                                          'Limite: R\$ ${card.limit.toStringAsFixed(2)}',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 14,
+                                          SizedBox(width: 8),
+                                          Expanded(
+                                            child: Text(
+                                              card.name,
+                                              style: TextStyle(
+                                                color: DefaultColors.black,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16,
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
                                           ),
-                                        ),
-                                        SizedBox(height: 8),
-                                        Row(
-                                          mainAxisAlignment: MainAxisAlignment.end,
-                                          children: [
-                                            IconButton(
-                                              icon: Icon(Icons.edit, color: Colors.white, size: 20),
-                                              constraints: BoxConstraints(),
-                                              padding: EdgeInsets.zero,
-                                              onPressed: () {
-                                                Get.to(() => AddCardPage(
-                                                      isEditing: true,
-                                                      card: card,
-                                                    ));
-                                              },
-                                            ),
-                                            SizedBox(width: 8),
-                                            IconButton(
-                                              icon: Icon(Icons.delete, color: Colors.white, size: 20),
-                                              constraints: BoxConstraints(),
-                                              padding: EdgeInsets.zero,
-                                              onPressed: () {
-                                                showDialog(
-                                                  context: context,
-                                                  builder: (context) => AlertDialog(
-                                                    title: Text('Confirmar exclusão'),
-                                                    content: Text('Tem certeza que deseja excluir o cartão ${card.name}?'),
-                                                    actions: [
-                                                      TextButton(
-                                                        child: Text('Cancelar'),
-                                                        onPressed: () {
-                                                          Navigator.of(context).pop();
-                                                        },
-                                                      ),
-                                                      TextButton(
-                                                        child: Text('Excluir'),
-                                                        onPressed: () {
-                                                          cardController.deleteCard(card.id!);
-                                                          Navigator.of(context).pop();
-                                                        },
-                                                      ),
-                                                    ],
-                                                  ),
-                                                );
-                                              },
-                                            ),
-                                          ],
-                                        )
-                                      ],
-                                    ),
+                                        ],
+                                      ),
+                                      Spacer(),
+                                      // Text(
+                                      //   'Limite: R\$ ${card.limit.toStringAsFixed(2)}',
+                                      //   style: TextStyle(
+                                      //     color: Colors.white,
+                                      //     fontSize: 14,
+                                      //   ),
+                                      // ),
+                                      SizedBox(height: 8),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          IconButton(
+                                            icon: Icon(Icons.edit, color: DefaultColors.black, size: 20),
+                                            constraints: BoxConstraints(),
+                                            padding: EdgeInsets.zero,
+                                            onPressed: () {
+                                              Get.to(() => AddCardPage(
+                                                    isEditing: true,
+                                                    card: card,
+                                                  ));
+                                            },
+                                          ),
+                                          SizedBox(width: 8),
+                                          IconButton(
+                                            icon: Icon(Icons.delete, color: DefaultColors.black, size: 20),
+                                            constraints: BoxConstraints(),
+                                            padding: EdgeInsets.zero,
+                                            onPressed: () {
+                                              showDialog(
+                                                context: context,
+                                                builder: (context) => AlertDialog(
+                                                  title: Text('Confirmar exclusão'),
+                                                  content: Text('Tem certeza que deseja excluir o cartão ${card.name}?'),
+                                                  actions: [
+                                                    TextButton(
+                                                      child: Text('Cancelar'),
+                                                      onPressed: () {
+                                                        Navigator.of(context).pop();
+                                                      },
+                                                    ),
+                                                    TextButton(
+                                                      child: Text('Excluir'),
+                                                      onPressed: () {
+                                                        cardController.deleteCard(card.id!);
+                                                        Navigator.of(context).pop();
+                                                      },
+                                                    ),
+                                                  ],
+                                                ),
+                                              );
+                                            },
+                                          ),
+                                        ],
+                                      )
+                                    ],
                                   ),
                                 ),
-                              );
-                            },
-                          ),
-                        );
-                      }
-                    }),
-                  ],
-                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  }),
+                ],
               ),
             ),
           ],
@@ -330,18 +325,25 @@ class _AddCardPageState extends State<AddCardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: DefaultColors.background,
       appBar: AppBar(
-        title: Text(widget.isEditing ? 'Editar Cartão' : 'Adicionar Cartão'),
+        backgroundColor: DefaultColors.background,
+        title: Text(widget.isEditing ? 'Editar Cartão' : 'Criar Cartão'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            DefaultTitleTransaction(title: "Nome do cartão"),
             TextField(
               controller: nameController,
               decoration: InputDecoration(
-                labelText: 'Nome do Cartão',
+                hintText: 'Nome do Cartão',
+                hintStyle: TextStyle(
+                  color: DefaultColors.grey,
+                  fontSize: 14.sp,
+                ),
                 border: OutlineInputBorder(),
               ),
             ),
