ef1e5225211fe3020e1f279b10025948b2787d5c
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-20 13:38:20 -0300

    ai ta lindo

--- lib/page/resume/resume_page.dart
+++ lib/page/resume/resume_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:organizamais/controller/transaction_controller.dart';
-
 import 'package:intl/intl.dart';
 import 'package:organizamais/utils/color.dart';
 
@@ -15,10 +14,31 @@ class ResumePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TransactionController transactionController = Get.put(TransactionController());
+    final selectedMonth = ''.obs;
 
-    String formatDate(String dateStr) {
+    // Lista fixa de todos os meses do ano atual
+    List<String> getAllMonths() {
+      List<String> months = [];
+      DateTime now = DateTime.now();
+      for (int i = 0; i < 12; i++) {
+        DateTime month = DateTime(now.year, i + 1);
+        String monthYear = DateFormat('MMMM/yyyy', 'pt_BR').format(month);
+        months.add(monthYear);
+      }
+      return months;
+    }
+
+    String getRelativeDate(String dateStr) {
       DateTime date = DateTime.parse(dateStr);
-      return DateFormat('dd/MM/yyyy').format(date);
+      DateTime now = DateTime.now();
+
+      if (DateUtils.isSameDay(date, now)) {
+        return 'Hoje';
+      } else if (DateUtils.isSameDay(date, now.subtract(const Duration(days: 1)))) {
+        return 'Ontem';
+      } else {
+        return DateFormat('dd/MM/yyyy').format(date);
+      }
     }
 
     String formatValue(dynamic value) {
@@ -30,31 +50,33 @@ class ResumePage extends StatelessWidget {
       return "R\$ 0.00";
     }
 
-    Map<String, List<dynamic>> groupTransactionsByDate(List transactions) {
+    Map<String, List<dynamic>> groupTransactionsByDate(List transactions, String selectedMonth) {
       Map<String, List<dynamic>> grouped = {};
 
       for (var transaction in transactions) {
-        String formattedDate = formatDate(transaction.paymentDay);
-        if (!grouped.containsKey(formattedDate)) {
-          grouped[formattedDate] = [];
+        DateTime transactionDate = DateTime.parse(transaction.paymentDay);
+        String transactionMonth = DateFormat('MMMM/yyyy', 'pt_BR').format(transactionDate);
+
+        if (selectedMonth.isNotEmpty && transactionMonth != selectedMonth) {
+          continue;
         }
-        grouped[formattedDate]!.add(transaction);
+
+        String relativeDate = getRelativeDate(transaction.paymentDay);
+        if (!grouped.containsKey(relativeDate)) {
+          grouped[relativeDate] = [];
+        }
+        grouped[relativeDate]!.add(transaction);
       }
 
       grouped.forEach((date, transactions) {
         transactions.sort((a, b) {
           DateTime dateTimeA = DateTime.parse(a.paymentDay);
           DateTime dateTimeB = DateTime.parse(b.paymentDay);
-          return dateTimeB.compareTo(dateTimeA); // Ordem decrescente
+          return dateTimeB.compareTo(dateTimeA);
         });
       });
 
-      return Map.fromEntries(grouped.entries.toList()
-        ..sort((a, b) {
-          DateTime dateA = DateFormat('dd/MM/yyyy').parse(a.key);
-          DateTime dateB = DateFormat('dd/MM/yyyy').parse(b.key);
-          return dateB.compareTo(dateA);
-        }));
+      return grouped;
     }
 
     return Scaffold(
@@ -67,12 +89,57 @@ class ResumePage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
+              // Mês buttons in a horizontal scrollable list
+              SizedBox(
+                height: 40.h,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: getAllMonths().length,
+                  separatorBuilder: (context, index) => SizedBox(width: 10.w),
+                  itemBuilder: (context, index) {
+                    final month = getAllMonths()[index];
+                    return Obx(() => ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: selectedMonth.value == month ? DefaultColors.green : DefaultColors.white,
+                            foregroundColor: selectedMonth.value == month ? DefaultColors.white : DefaultColors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.r),
+                            ),
+                          ),
+                          onPressed: () {
+                            if (selectedMonth.value == month) {
+                              selectedMonth.value = '';
+                            } else {
+                              selectedMonth.value = month;
+                            }
+                          },
+                          child: Text(
+                            month.split('/')[0].capitalize!, // Apenas o nome do mês com primeira letra maiúscula
+                            style: TextStyle(fontSize: 12.sp),
+                          ),
+                        ));
+                  },
+                ),
+              ),
+              SizedBox(height: 20.h),
+
+              // Transactions list
               Obx(() {
                 if (transactionController.transaction.isEmpty) {
-                  return DefaultTextNotTransaction();
+                  return const DefaultTextNotTransaction();
                 }
 
-                final groupedTransactions = groupTransactionsByDate(transactionController.transaction);
+                final groupedTransactions = groupTransactionsByDate(transactionController.transaction, selectedMonth.value);
+
+                if (groupedTransactions.isEmpty) {
+                  return Text(
+                    "Nenhuma transação encontrada para este período",
+                    style: TextStyle(
+                      color: DefaultColors.grey,
+                      fontSize: 14.sp,
+                    ),
+                  );
+                }
 
                 return ListView.separated(
                   shrinkWrap: true,
@@ -102,15 +169,11 @@ class ResumePage extends StatelessWidget {
                           itemCount: transactions.length,
                           itemBuilder: (context, index) {
                             final transaction = transactions[index];
-                            final DateTime transactionTime = DateTime.parse(transaction.paymentDay);
-                            DateFormat('HH:mm').format(transactionTime);
 
                             return Container(
                               decoration: BoxDecoration(
                                 color: DefaultColors.white,
-                                borderRadius: BorderRadius.circular(
-                                  24.r,
-                                ),
+                                borderRadius: BorderRadius.circular(24.r),
                               ),
                               padding: EdgeInsets.symmetric(
                                 vertical: 16.h,
@@ -178,7 +241,7 @@ class ResumePage extends StatelessWidget {
                     );
                   },
                 );
-              })
+              }),
             ],
           ),
         ),
@@ -188,9 +251,7 @@ class ResumePage extends StatelessWidget {
 }
 
 class DefaultTextNotTransaction extends StatelessWidget {
-  const DefaultTextNotTransaction({
-    super.key,
-  });
+  const DefaultTextNotTransaction({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -222,9 +283,7 @@ class DefaultTextNotTransaction extends StatelessWidget {
                 color: DefaultColors.green,
                 borderRadius: BorderRadius.circular(18.r),
               ),
-              padding: EdgeInsets.all(
-                5.w,
-              ),
+              padding: EdgeInsets.all(5.w),
               child: Icon(
                 Iconsax.add,
                 color: DefaultColors.white,
