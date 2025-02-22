de300140b3f7c23862ea49c5fe4bd730fcb67382
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-02-22 13:44:10 -0300

    .

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 
 class DefaultColors {
-  static const backgroundLight = Color(0xFFF6F6F6);
-  static const grey = Color(0xFF808080);
-  static const greyLight = Color(0xFF91919F);
-  static const black = Color(0xFF000000);
+  static const backgroundLight = Color(0xFFf6f6f6);
+  static const grey = Color(0xFF9E9E9E);
+  static const greyLight = Color(0xFFBDBDBD);
+  static const black = Color(0xFF212121);
   static const white = Color(0xFFFFFFFF);
-  static const green = Color(0xFF50bb77);
-  static const red = Color(0xFFFD3C4A);
+  static const green = Color(0xFF66BB6A);
+  static const red = Color(0xFFEF5350);
 
-  static const backgroundDark = Color(0xFF1A1919);
-  static const backgroundCard = Color(0xFF242424);
-  static const backgroundgreyDark = Color(0xFF606455);
+  static const backgroundDark = Color(0xFF121212);
+  static const backgroundCard = Color(0xFF1E1E1E);
+  static const backgroundgreyDark = Color(0xFF757575);
 
   // Cores das categorias
-  static const blue = Color(0xFF4A90E2); // Casa
-  static const orange = Color(0xFFF5A623); // Alimentação
-  static const darkGrey = Color(0xFF7B7B7B); // Transporte
-  static const purple = Color(0xFF8E44AD); // Educação
-  static const brightRed = Color(0xFFE74C3C); // Saúde
-  static const pink = Color(0xFFFF6B81); // Beleza e Bem-Estar
-  static const teal = Color(0xFF26A69A); // Compras e Lazer
-  static const darkBlue = Color(0xFF34495E); // Trabalho e Negócios
-  static const gold = Color(0xFFF39C12); // Finanças e Investimentos
-  static const navy = Color(0xFF2C3E50);
-  static const orangeDark = Color(0xFFFF7F50);
+  static const blue = Color(0xFF64B5F6); // Casa
+  static const orange = Color(0xFFFFCC80); // Alimentação
+  static const darkGrey = Color(0xFF9E9E9E); // Transporte
+  static const purple = Color(0xFFBA68C8); // Educação
+  static const brightRed = Color(0xFFEF9A9A); // Saúde
+  static const pink = Color(0xFFF48FB1); // Beleza e Bem-Estar
+  static const teal = Color(0xFF80CBC4); // Compras e Lazer
+  static const darkBlue = Color(0xFF7986CB); // Trabalho e Negócios
+  static const gold = Color(0xFFFFE082); // Finanças e Investimentos
+  static const navy = Color(0xFF546E7A);
+  static const orangeDark = Color(0xFFFFAB91);
 }
