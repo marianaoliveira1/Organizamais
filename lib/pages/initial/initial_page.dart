86afa9433b547aab5f58d3981923b5121d1aa027
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-03-13 16:20:20 -0300

    .

--- lib/pages/initial/initial_page.dart
+++ lib/pages/initial/initial_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:organizamais/pages/initial/widget/finance_summary.dart';
 
-import '../../controller/auth_controller.dart';
 import '../../controller/fixed_accounts_controller.dart';
 
 import 'widget/credit_card_selection.dart';
+import 'widget/finance_summary.dart';
 import 'widget/widghet_fixed_accoutns.dart';
 
 class InitialPage extends StatelessWidget {
@@ -34,56 +33,15 @@ class InitialPage extends StatelessWidget {
                     FinanceSummaryWidget(),
                     DefaultWidgetFixedAccounts(),
                     CreditCardSection(),
+                    SizedBox(
+                      height: 10.h,
+                    ),
                   ],
                 ),
               ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class DefaultDrawer extends StatelessWidget {
-  const DefaultDrawer({
-    super.key,
-    required this.authController,
-  });
-
-  final AuthController authController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          ListTile(
-            title: Text("Perfil"),
-            onTap: () {
-              Get.toNamed("/profile");
-            },
-          ),
-          ListTile(
-            title: Text("Mues cartões"),
-            onTap: () {
-              Get.toNamed("/profile");
-            },
-          ),
-          ListTile(
-            title: Text("Minhas contas fixas"),
-            onTap: () {
-              Get.toNamed("/profile");
-            },
-          ),
-          Spacer(),
-          ListTile(
-            title: Text("Sair"),
-            onTap: () {
-              authController.logout();
-            },
-          ),
-        ],
       ),
     );
   }
