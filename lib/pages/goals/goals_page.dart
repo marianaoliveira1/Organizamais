3994f224ec69996a45393efa9c5146d6bd69f79c
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-03-19 16:07:06 -0300

    .

--- lib/pages/goals/goals_page.dart
+++ lib/pages/goals/goals_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:intl/intl.dart';
-
 import '../../controller/goal_controller.dart';
 import '../../model/goal_model.dart';
 import '../transaction/pages/category_page.dart';
@@ -11,40 +12,297 @@ import 'pages/details_goals_page.dart';
 class GoalsPage extends StatelessWidget {
   final GoalController goalController = Get.put(GoalController());
 
+  GoalsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     goalController.startGoalStream();
-    return Scaffold(
-      appBar: AppBar(title: Text('Minhas Metas')),
-      body: Obx(() {
-        if (goalController.goal.isEmpty) {
-          return Center(child: Text('Nenhuma meta encontrada.'));
-        }
-        return ListView.builder(
-          itemCount: goalController.goal.length,
-          itemBuilder: (context, index) {
-            final GoalModel goal = goalController.goal[index];
-            final String cleanedValue = goal.value.replaceAll(RegExp(r'[^\d\.]'), '').replaceAll(',', '.');
-            final double numericValue = double.tryParse(cleanedValue) ?? 0.0;
-            final double progress = goal.currentValue / numericValue;
-            final category = findCategoryById(goal.categoryId);
 
-            return ListTile(
-              leading: category != null ? Image.asset(category['icon'], height: 30, width: 30) : Icon(Icons.category),
-              title: Text(goal.name),
-              subtitle: Text(
-                '${NumberFormat.currency(locale: 'pt_BR').format(goal.currentValue)} / ${NumberFormat.currency(locale: 'pt_BR').format(numericValue)}',
+    return Scaffold(
+      backgroundColor: theme.scaffoldBackgroundColor,
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20.w,
+          vertical: 30.h,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Metas',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.bold,
+                    color: theme.primaryColor,
+                  ),
+                ),
+                InkWell(
+                  onTap: () => Get.to(
+                    () => AddGoalPage(),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: theme.cardColor,
+                      borderRadius: BorderRadius.circular(60.r),
+                    ),
+                    padding: EdgeInsets.all(6.h),
+                    child: Icon(Iconsax.add, size: 16.sp),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              'Atinja seus objetivos financeiros mais rápido',
+              style: TextStyle(
+                fontSize: 12.sp,
+                color: Colors.grey,
               ),
-              trailing: CircularProgressIndicator(value: progress),
-              onTap: () => Get.to(() => GoalDetailsPage(initialGoal: goal)), // Corrected line
-            );
-          },
-        );
-      }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.to(() => AddGoalPage()),
-        child: Icon(Icons.add),
+            ),
+            Expanded(
+              child: Obx(() {
+                if (goalController.goal.isEmpty) {
+                  return Center(
+                    child: Text(
+                      'Nenhuma meta encontrada.',
+                      style: TextStyle(
+                        color: theme.primaryColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: goalController.goal.length,
+                  itemBuilder: (context, index) {
+                    final GoalModel goal = goalController.goal[index];
+                    final String cleanedValue = goal.value.replaceAll(RegExp(r'[^\d\.]'), '').replaceAll(',', '.');
+                    final double numericValue = double.tryParse(cleanedValue) ?? 0.0;
+                    final double progress = goal.currentValue / numericValue;
+                    final category = findCategoryById(goal.categoryId);
+                    final currentDate = DateTime.now();
+                    final formattedDate = "${currentDate.day}, ${_getMonthName(currentDate.month)}. ${currentDate.year}";
+
+                    return InkWell(
+                      onTap: () => Get.to(() => GoalDetailsPage(initialGoal: goal)),
+                      child: Container(
+                        margin: EdgeInsets.only(bottom: 16.h),
+                        decoration: BoxDecoration(
+                          color: theme.cardColor,
+                          borderRadius: BorderRadius.circular(16.r),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(16.w),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      category != null
+                                          ? Image.asset(
+                                              category['icon'],
+                                              height: 28.h,
+                                              width: 28.w,
+                                            )
+                                          : Container(
+                                              width: 28.w,
+                                              height: 28.h,
+                                              decoration: BoxDecoration(
+                                                color: Colors.amber.shade800,
+                                                shape: BoxShape.circle,
+                                              ),
+                                              child: Icon(
+                                                Icons.category,
+                                                color: Colors.white,
+                                                size: 24.sp,
+                                              ),
+                                            ),
+                                      SizedBox(width: 16.w),
+                                      Text(
+                                        goal.name,
+                                        style: TextStyle(
+                                          fontSize: 14.sp,
+                                          fontWeight: FontWeight.bold,
+                                          color: theme.primaryColor,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    formattedDate,
+                                    style: TextStyle(
+                                      fontSize: 13.sp,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 16.h),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "R\$ ${goal.currentValue.toStringAsFixed(2).replaceAll('.', ',')}",
+                                    style: TextStyle(
+                                      fontSize: 12.sp,
+                                      fontWeight: FontWeight.bold,
+                                      color: theme.primaryColor,
+                                    ),
+                                  ),
+                                  Text(
+                                    "R\$ ${numericValue.toStringAsFixed(2).replaceAll('.', ',')}",
+                                    style: TextStyle(
+                                      fontSize: 12.sp,
+                                      fontWeight: FontWeight.bold,
+                                      color: theme.primaryColor,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 8.h),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(8.r),
+                                child: LinearProgressIndicator(
+                                  value: progress,
+                                  backgroundColor: Colors.grey.shade300,
+                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                                  minHeight: 6.h,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  String _getMonthName(int month) {
+    final List<String> months = [
+      'jan',
+      'fev',
+      'mar',
+      'abr',
+      'mai',
+      'jun',
+      'jul',
+      'ago',
+      'set',
+      'out',
+      'nov',
+      'dez'
+    ];
+    return months[month - 1];
+  }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:get/get.dart';
+// import 'package:intl/intl.dart';
+
+// import '../../controller/goal_controller.dart';
+// import '../../model/goal_model.dart';
+// import '../transaction/pages/category_page.dart';
+// import 'pages/add_goal_page.dart';
+// import 'pages/details_goals_page.dart';
+
+// class GoalsPage extends StatelessWidget {
+//   final GoalController goalController = Get.put(GoalController());
+
+//   GoalsPage({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final theme = Theme.of(context);
+//     goalController.startGoalStream();
+
+//     return Scaffold(
+//       backgroundColor: theme.scaffoldBackgroundColor,
+//       body: Padding(
+//         padding: EdgeInsets.symmetric(
+//           vertical: 20.w,
+//           horizontal: 20.h,
+//         ),
+//         child: Obx(() {
+//           if (goalController.goal.isEmpty) {
+//             return Center(
+//               child: Text(
+//                 'Nenhuma meta encontrada.',
+//                 style: TextStyle(
+//                   color: theme.primaryColor,
+//                   fontSize: 14.sp,
+//                   fontWeight: FontWeight.w500,
+//                 ),
+//               ),
+//             );
+//           }
+//           return ListView.builder(
+//             itemCount: goalController.goal.length,
+//             itemBuilder: (context, index) {
+//               final GoalModel goal = goalController.goal[index];
+//               final String cleanedValue = goal.value.replaceAll(RegExp(r'[^\d\.]'), '').replaceAll(',', '.');
+//               final double numericValue = double.tryParse(cleanedValue) ?? 0.0;
+//               final double progress = goal.currentValue / numericValue;
+//               final category = findCategoryById(goal.categoryId);
+
+//               return Container(
+//                 decoration: BoxDecoration(
+//                   color: theme.cardColor,
+//                   borderRadius: BorderRadius.circular(24.r),
+//                 ),
+//                 child: ListTile(
+//                   leading: category != null
+//                       ? Image.asset(
+//                           category['icon'],
+//                           height: 30.h,
+//                           width: 30.h,
+//                         )
+//                       : Icon(
+//                           Icons.category,
+//                         ),
+//                   title: Text(
+//                     goal.name,
+//                     style: TextStyle(
+//                       fontSize: 14.sp,
+//                       fontWeight: FontWeight.w500,
+//                       color: theme.primaryColor,
+//                     ),
+//                   ),
+//                   subtitle: Text(
+//                     '${NumberFormat.currency(locale: 'pt_BR').format(goal.currentValue)} / ${NumberFormat.currency(locale: 'pt_BR').format(
+//                       numericValue,
+//                     )}',
+//                   ),
+//                   trailing: CircularProgressIndicator(value: progress),
+//                   onTap: () => Get.to(() => GoalDetailsPage(initialGoal: goal)),
+//                 ),
+//               );
+//             },
+//           );
+//         }),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () => Get.to(() => AddGoalPage()),
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
