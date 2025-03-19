43a6d5b16c048ac270dbc88f4b43db4b0fc472ab
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2025-03-19 17:22:05 -0300

    .

--- lib/pages/goals/pages/add_goal_page.dart
+++ lib/pages/goals/pages/add_goal_page.dart
@@ -1,13 +1,21 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
 import '../../../controller/goal_controller.dart';
 import '../../../model/goal_model.dart';
 import '../../../model/transaction_model.dart';
+import '../../../utils/color.dart';
+import '../../transaction/transaction_page.dart';
 import '../../transaction/widget/button_select_category.dart';
+import '../../transaction/widget/title_transaction.dart';
 
 class AddGoalPage extends StatefulWidget {
+  const AddGoalPage({super.key});
+
   @override
   _AddGoalPageState createState() => _AddGoalPageState();
 }
@@ -35,22 +43,127 @@ class _AddGoalPageState extends State<AddGoalPage> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Adicionar Meta')),
+      backgroundColor: theme.scaffoldBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: theme.scaffoldBackgroundColor,
+        title: Text(
+          'Adicionar Meta',
+          style: TextStyle(color: theme.primaryColor),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          spacing: 10.h,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(controller: nameController, decoration: InputDecoration(labelText: 'Nome')),
+            DefaultTitleTransaction(
+              title: "Titulo",
+            ),
+            TextField(
+              controller: nameController,
+              style: TextStyle(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w500,
+                color: theme.primaryColor,
+              ),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(
+                    12.r,
+                  ),
+                  borderSide: BorderSide(
+                    color: theme.primaryColor.withOpacity(.5),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(
+                    12.r,
+                  ),
+                  borderSide: BorderSide(
+                    color: theme.primaryColor.withOpacity(.5),
+                  ),
+                ),
+                hintText: 'ex: Comprar um carro',
+                hintStyle: TextStyle(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w500,
+                  color: theme.primaryColor.withOpacity(.5),
+                ),
+              ),
+            ),
+            DefaultTitleTransaction(
+              title: "Valor",
+            ),
             TextField(
               controller: valueController,
-              decoration: InputDecoration(labelText: 'Valor'),
+              style: TextStyle(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w500,
+                color: theme.primaryColor,
+              ),
               keyboardType: TextInputType.number,
+              inputFormatters: [
+                CurrencyInputFormatter(),
+              ],
+              decoration: InputDecoration(
+                fillColor: theme.primaryColor,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(
+                    12.r,
+                  ),
+                ),
+                prefixIcon: Icon(
+                  Icons.attach_money,
+                ),
+                prefixIconColor: DefaultColors.grey,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(
+                    12.r,
+                  ),
+                  borderSide: BorderSide(
+                    color: theme.primaryColor.withOpacity(.5),
+                  ),
+                ),
+                focusColor: theme.primaryColor,
+                hintText: "0,00",
+                hintStyle: TextStyle(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w500,
+                  color: theme.primaryColor.withOpacity(0.5),
+                ),
+              ),
             ),
-            ListTile(
-              title: Text("Data: ${DateFormat('dd/MM/yyyy').format(selectedDate)}"),
-              trailing: Icon(Icons.calendar_today),
+            DefaultTitleTransaction(
+              title: "Data",
+            ),
+            GestureDetector(
               onTap: () => _selectDate(context),
+              child: Container(
+                padding: EdgeInsets.all(
+                  16.r,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.r),
+                  border: Border.all(
+                    color: theme.primaryColor.withOpacity(.5),
+                  ),
+                ),
+                child: Text(
+                  "Data: ${DateFormat('dd/MM/yyyy').format(selectedDate)}",
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w500,
+                    color: theme.primaryColor,
+                  ),
+                ),
+              ),
+            ),
+            DefaultTitleTransaction(
+              title: "Categoria",
             ),
             DefaultButtonSelectCategory(
               selectedCategory: categoryId,
@@ -61,8 +174,9 @@ class _AddGoalPageState extends State<AddGoalPage> {
               },
               transactionType: TransactionType.despesa,
             ),
-            ElevatedButton(
-              onPressed: () {
+            Spacer(),
+            InkWell(
+              onTap: () {
                 final double value = double.tryParse(valueController.text.replaceAll(RegExp(r'[^\d\.]'), '').replaceAll(',', '.')) ?? 0.0;
                 final goal = GoalModel(
                   name: nameController.text,
@@ -74,7 +188,25 @@ class _AddGoalPageState extends State<AddGoalPage> {
                 goalController.addGoal(goal);
                 Get.back();
               },
-              child: Text('Salvar'),
+              child: Container(
+                padding: EdgeInsets.all(
+                  16.r,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.r),
+                  border: Border.all(
+                    color: theme.primaryColor.withOpacity(.5),
+                  ),
+                ),
+                child: Text(
+                  "Salvar",
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w500,
+                    color: theme.primaryColor,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
